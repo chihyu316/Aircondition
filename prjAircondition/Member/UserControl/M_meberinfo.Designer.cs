e08239fc4cde8fbedfffdaa326920df0df491058
Nathaniel829 <andy.lien.home@gmail.com>
2025-06-02 15:36:14 +0800

    會員資料介面

--- prjAircondition/Member/UserControl/M_meberinfo.Designer.cs
+++ prjAircondition/Member/UserControl/M_meberinfo.Designer.cs
@@ -28,10 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(M_meberinfo));
+            this.MemberListBox = new System.Windows.Forms.ListBox();
+            this.SaveBTN = new System.Windows.Forms.Button();
+            this.ShowPasswordBTN = new System.Windows.Forms.Button();
+            this.Accountlbl = new System.Windows.Forms.Label();
+            this.NicknameTXB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.FemaleCheckBox = new System.Windows.Forms.CheckBox();
+            this.MaleCheckBox = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PhoneNumberTXB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PassWordTXB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AvatarBOX = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarBOX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MemberListBox
+            // 
+            this.MemberListBox.Font = new System.Drawing.Font("新細明體", 15F);
+            this.MemberListBox.FormattingEnabled = true;
+            this.MemberListBox.ItemHeight = 20;
+            this.MemberListBox.Location = new System.Drawing.Point(3, 92);
+            this.MemberListBox.Name = "MemberListBox";
+            this.MemberListBox.Size = new System.Drawing.Size(273, 304);
+            this.MemberListBox.TabIndex = 40;
+            // 
+            // SaveBTN
+            // 
+            this.SaveBTN.Location = new System.Drawing.Point(937, 361);
+            this.SaveBTN.Name = "SaveBTN";
+            this.SaveBTN.Size = new System.Drawing.Size(83, 39);
+            this.SaveBTN.TabIndex = 39;
+            this.SaveBTN.Text = "儲存";
+            this.SaveBTN.UseVisualStyleBackColor = true;
+            // 
+            // ShowPasswordBTN
+            // 
+            this.ShowPasswordBTN.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ShowPasswordBTN.BackgroundImage")));
+            this.ShowPasswordBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ShowPasswordBTN.Location = new System.Drawing.Point(928, 166);
+            this.ShowPasswordBTN.Name = "ShowPasswordBTN";
+            this.ShowPasswordBTN.Size = new System.Drawing.Size(29, 31);
+            this.ShowPasswordBTN.TabIndex = 38;
+            this.ShowPasswordBTN.Text = "\r\n";
+            this.ShowPasswordBTN.UseVisualStyleBackColor = true;
+            // 
+            // Accountlbl
+            // 
+            this.Accountlbl.AutoSize = true;
+            this.Accountlbl.Font = new System.Drawing.Font("新細明體", 15F);
+            this.Accountlbl.Location = new System.Drawing.Point(770, 108);
+            this.Accountlbl.Name = "Accountlbl";
+            this.Accountlbl.Size = new System.Drawing.Size(0, 20);
+            this.Accountlbl.TabIndex = 37;
+            // 
+            // NicknameTXB
+            // 
+            this.NicknameTXB.Font = new System.Drawing.Font("新細明體", 15F);
+            this.NicknameTXB.Location = new System.Drawing.Point(757, 371);
+            this.NicknameTXB.Name = "NicknameTXB";
+            this.NicknameTXB.Size = new System.Drawing.Size(175, 31);
+            this.NicknameTXB.TabIndex = 36;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label5.Location = new System.Drawing.Point(635, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 27);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "暱稱";
+            // 
+            // FemaleCheckBox
+            // 
+            this.FemaleCheckBox.AutoSize = true;
+            this.FemaleCheckBox.Font = new System.Drawing.Font("新細明體", 15F);
+            this.FemaleCheckBox.Location = new System.Drawing.Point(842, 232);
+            this.FemaleCheckBox.Name = "FemaleCheckBox";
+            this.FemaleCheckBox.Size = new System.Drawing.Size(48, 24);
+            this.FemaleCheckBox.TabIndex = 34;
+            this.FemaleCheckBox.Text = "女";
+            this.FemaleCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MaleCheckBox
+            // 
+            this.MaleCheckBox.AutoSize = true;
+            this.MaleCheckBox.Font = new System.Drawing.Font("新細明體", 15F);
+            this.MaleCheckBox.Location = new System.Drawing.Point(774, 232);
+            this.MaleCheckBox.Name = "MaleCheckBox";
+            this.MaleCheckBox.Size = new System.Drawing.Size(48, 24);
+            this.MaleCheckBox.TabIndex = 33;
+            this.MaleCheckBox.Text = "男";
+            this.MaleCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label4.Location = new System.Drawing.Point(635, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 27);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "性別:";
+            // 
+            // PhoneNumberTXB
+            // 
+            this.PhoneNumberTXB.Font = new System.Drawing.Font("新細明體", 15F);
+            this.PhoneNumberTXB.Location = new System.Drawing.Point(757, 293);
+            this.PhoneNumberTXB.Name = "PhoneNumberTXB";
+            this.PhoneNumberTXB.Size = new System.Drawing.Size(175, 31);
+            this.PhoneNumberTXB.TabIndex = 31;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label3.Location = new System.Drawing.Point(619, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 27);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "電話號碼:";
+            // 
+            // PassWordTXB
+            // 
+            this.PassWordTXB.Font = new System.Drawing.Font("新細明體", 15F);
+            this.PassWordTXB.Location = new System.Drawing.Point(757, 166);
+            this.PassWordTXB.Name = "PassWordTXB";
+            this.PassWordTXB.PasswordChar = '●';
+            this.PassWordTXB.Size = new System.Drawing.Size(175, 31);
+            this.PassWordTXB.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label2.Location = new System.Drawing.Point(635, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 27);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "密碼:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Location = new System.Drawing.Point(635, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 27);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "帳號:";
+            // 
+            // AvatarBOX
+            // 
+            this.AvatarBOX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AvatarBOX.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.AvatarBOX.Location = new System.Drawing.Point(301, 98);
+            this.AvatarBOX.Name = "AvatarBOX";
+            this.AvatarBOX.Size = new System.Drawing.Size(312, 298);
+            this.AvatarBOX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.AvatarBOX.TabIndex = 26;
+            this.AvatarBOX.TabStop = false;
+            // 
+            // M_meberinfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.MemberListBox);
+            this.Controls.Add(this.SaveBTN);
+            this.Controls.Add(this.ShowPasswordBTN);
+            this.Controls.Add(this.Accountlbl);
+            this.Controls.Add(this.NicknameTXB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.FemaleCheckBox);
+            this.Controls.Add(this.MaleCheckBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PhoneNumberTXB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PassWordTXB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AvatarBOX);
+            this.Name = "M_meberinfo";
+            this.Size = new System.Drawing.Size(1028, 586);
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarBOX)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox MemberListBox;
+        private System.Windows.Forms.Button SaveBTN;
+        private System.Windows.Forms.Button ShowPasswordBTN;
+        private System.Windows.Forms.Label Accountlbl;
+        private System.Windows.Forms.TextBox NicknameTXB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox FemaleCheckBox;
+        private System.Windows.Forms.CheckBox MaleCheckBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PhoneNumberTXB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PassWordTXB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox AvatarBOX;
     }
 }
