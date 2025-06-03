ee42fb325d263e5c7440ddc970ea6a200f6c299a
kokorokara <508170117@m365.fju.edu.tw>
2025-06-03 14:57:40 +0800

    師傅版面更新 v3
    師傅版面更新 v3

--- prjAircondition/Tech/UserControl/T_OrderUserControl1.Designer.cs
+++ prjAircondition/Tech/UserControl/T_OrderUserControl1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace prjAircondition.Tech
+﻿using prjAircondition.Tech.DataSet;
+
+namespace prjAircondition.Tech
 {
     partial class T_OrderUserControl1
     {
@@ -28,10 +30,200 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        }
+            this.components = new System.ComponentModel.Container();
+            this.t_ACDataSet1 = new prjAircondition.Tech.DataSet.T_ACDataSet();
+            this.workTypeBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.techniciansTableAdapter1 = new prjAircondition.Tech.DataSet.T_ACDataSetTableAdapters.TechniciansTableAdapter();
+            this.workTypeTableAdapter1 = new prjAircondition.Tech.DataSet.T_ACDataSetTableAdapters.WorkTypeTableAdapter();
+            this.workOrderTableAdapter1 = new prjAircondition.Tech.DataSet.T_ACDataSetTableAdapters.WorkOrderTableAdapter();
+            this.workOrderlabel = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.WorkOrderDataGridView = new System.Windows.Forms.DataGridView();
+            this.WorkTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.labelTechName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.workType = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.workOrderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.t_ACDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.workTypeBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WorkOrderDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.workOrderBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // t_ACDataSet1
+            // 
+            this.t_ACDataSet1.DataSetName = "T_ACDataSet";
+            this.t_ACDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // techniciansTableAdapter1
+            // 
+            this.techniciansTableAdapter1.ClearBeforeFill = true;
+            // 
+            // workTypeTableAdapter1
+            // 
+            this.workTypeTableAdapter1.ClearBeforeFill = true;
+            // 
+            // workOrderTableAdapter1
+            // 
+            this.workOrderTableAdapter1.ClearBeforeFill = true;
+            // 
+            // workOrderlabel
+            // 
+            this.workOrderlabel.AutoSize = true;
+            this.workOrderlabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workOrderlabel.Location = new System.Drawing.Point(297, 249);
+            this.workOrderlabel.Name = "workOrderlabel";
+            this.workOrderlabel.Size = new System.Drawing.Size(64, 28);
+            this.workOrderlabel.TabIndex = 16;
+            this.workOrderlabel.Text = "1/10";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(518, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 43);
+            this.button4.TabIndex = 12;
+            this.button4.Text = ">>";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(163, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 43);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(394, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 43);
+            this.button3.TabIndex = 14;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(37, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 43);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // WorkOrderDataGridView
+            // 
+            this.WorkOrderDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.WorkOrderDataGridView.Location = new System.Drawing.Point(30, 324);
+            this.WorkOrderDataGridView.Name = "WorkOrderDataGridView";
+            this.WorkOrderDataGridView.RowHeadersWidth = 62;
+            this.WorkOrderDataGridView.RowTemplate.Height = 31;
+            this.WorkOrderDataGridView.Size = new System.Drawing.Size(801, 531);
+            this.WorkOrderDataGridView.TabIndex = 10;
+            // 
+            // WorkTypeComboBox
+            // 
+            this.WorkTypeComboBox.FormattingEnabled = true;
+            this.WorkTypeComboBox.Location = new System.Drawing.Point(210, 181);
+            this.WorkTypeComboBox.Name = "WorkTypeComboBox";
+            this.WorkTypeComboBox.Size = new System.Drawing.Size(306, 26);
+            this.WorkTypeComboBox.TabIndex = 11;
+            // 
+            // labelTechName
+            // 
+            this.labelTechName.AutoSize = true;
+            this.labelTechName.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTechName.Location = new System.Drawing.Point(201, 108);
+            this.labelTechName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTechName.Name = "labelTechName";
+            this.labelTechName.Size = new System.Drawing.Size(165, 37);
+            this.labelTechName.TabIndex = 6;
+            this.labelTechName.Text = "師傅名稱";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 37);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "師傅:";
+            // 
+            // workType
+            // 
+            this.workType.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workType.Location = new System.Drawing.Point(38, 177);
+            this.workType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.workType.Name = "workType";
+            this.workType.Size = new System.Drawing.Size(165, 37);
+            this.workType.TabIndex = 8;
+            this.workType.Text = "工作類型";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 37);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "訂單資料";
+            // 
+            // T_OrderUserControl1
+            // 
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.Controls.Add(this.workOrderlabel);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.WorkOrderDataGridView);
+            this.Controls.Add(this.WorkTypeComboBox);
+            this.Controls.Add(this.labelTechName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.workType);
+            this.Controls.Add(this.label1);
+            this.Name = "T_OrderUserControl1";
+            this.Size = new System.Drawing.Size(1215, 1075);
+            ((System.ComponentModel.ISupportInitialize)(this.t_ACDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.workTypeBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WorkOrderDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.workOrderBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
+        }
         #endregion
+
+        private T_ACDataSet t_ACDataSet1;
+        private System.Windows.Forms.BindingSource workTypeBindingSource1;
+        private DataSet.T_ACDataSetTableAdapters.TechniciansTableAdapter techniciansTableAdapter1;
+        private DataSet.T_ACDataSetTableAdapters.WorkTypeTableAdapter workTypeTableAdapter1;
+        private DataSet.T_ACDataSetTableAdapters.WorkOrderTableAdapter workOrderTableAdapter1;
+        private System.Windows.Forms.Label workOrderlabel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView WorkOrderDataGridView;
+        private System.Windows.Forms.ComboBox WorkTypeComboBox;
+        private System.Windows.Forms.Label labelTechName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label workType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource workOrderBindingSource;
     }
-}
+}   
--- prjAircondition/Tech/UserControl/T_OrderUserControl1.cs
+++ prjAircondition/Tech/UserControl/T_OrderUserControl1.cs
@@ -12,9 +12,162 @@ namespace prjAircondition.Tech
 {
     public partial class T_OrderUserControl1 : UserControl
     {
+        //傳進來的師傅編號
+        public string TechAccount { get; set; }
+
+        //要配對的師傅id
+        public int techID;
+
+        //不過這邊要用id判斷訂單資料
         public T_OrderUserControl1()
         {
             InitializeComponent();
+        }
+
+        private void OrderUserControl1_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TechAccount))
+            {
+                //先取得那 師傅資料欄位中的id 再來做載入訂單動作
+                techID = GetTechnicianIDByAccount(TechAccount);
+                //載入師傅的訂單
+                LoadOrders(techID);
+
+                // 先把 離線 師傅資料表 以帳號叫出來
+                //把欄位name抓出來給labelTechName.Text
+                this.techniciansTableAdapter1.FillByAccount(this.t_ACDataSet1.Technicians, TechAccount);
+                if (this.t_ACDataSet1.Technicians.Rows.Count > 0)
+                {
+                    string techName = this.t_ACDataSet1.Technicians[0].name;
+                    labelTechName.Text = techName;
+                }
+            }
+
+            //串好workOrder中介點
+            // 先在 Form_Load (UserControl_Load) 中做綁定：
+
+            this.workOrderBindingSource.DataSource = this.t_ACDataSet1.WorkOrder;
+            this.WorkOrderDataGridView.DataSource = this.workOrderBindingSource;
+
+            //載入工作類型資料表
+            // 這裡是把 WorkTypes 資料撈進 DataSet
+            this.workTypeTableAdapter1.Fill(this.t_ACDataSet1.WorkType);
+
+            //設定binding
+            BindWorkTypeComboBox();
+        }
+
+        private void BindWorkTypeComboBox()
+        {
+            //先在comboBox加入全部選項
+            // 先在資料載入後插入一筆「全部」
+            DataRow newRow = this.t_ACDataSet1.WorkType.NewRow();
+            //增加一筆 資料含有這兩個欄位到這個資料表
+            newRow["WorkTypeID"] = 0;  // 0 代表全部
+            newRow["WorkType"] = "全部";
+            this.t_ACDataSet1.WorkType.Rows.InsertAt(newRow, 0);
+
+            //先綁好資料表
+            this.workTypeBindingSource1.DataSource = this.t_ACDataSet1.WorkType;
+
+            // 讓 ComboBox 綁定離線 這個資料中界點
+            WorkTypeComboBox.DataSource = this.workTypeBindingSource1;
+            // 給使用者看的文字 比如 1-維修 2-安裝
+            this.WorkTypeComboBox.DisplayMember = "WorkType";
+            //實際上拿到的是ID
+            WorkTypeComboBox.ValueMember = "WorkTypeID";      // 實際代表的值 (當 SelectedValue 取出時拿到的是 WorkTypeID)
+        }
+
+        //去撈索引 comboBox去撈對應的工單類型資料
+        private void WorkTypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (WorkTypeComboBox.SelectedValue != null)
+            {
+                //先初始化為0
+                int workTypeID = 0;
+                // 這種情況表示你 DataSource 綁在 BindingSource (也就是 workTypeBindingSource1)
+                // 你需要從 DataRowView 取出 WorkTypeID 欄位值
+                // 綁定int型別的欄位 轉object 因為裡面可能是Null或別的只能用Convert.toInt32
+                // 底下錯誤示範
+                // int workTypeID = (int)WorkTypeComboBox.SelectedValue;
+
+                //dateRow物件要[""] 欄位轉換成int
+                if (WorkTypeComboBox.SelectedItem is DataRowView drv)
+                {
+                    workTypeID = Convert.ToInt32(drv["WorkTypeID"]);
+                }
+                else
+                {
+                    workTypeID = Convert.ToInt32(WorkTypeComboBox.SelectedValue);
+                }
+
+                if (workTypeID == 0)
+                {
+                    // 如果選擇全部 -> 撈取該技師的所有訂單
+                    this.workOrderTableAdapter1.FillByTechnicianID(this.t_ACDataSet1.WorkOrder, techID);
+                }
+                else
+                {
+                    // 否則正常按照 typeID 去撈
+                    this.workOrderTableAdapter1.FillByTechnicianIDAndWorkTypeID(this.t_ACDataSet1.WorkOrder, techID, workTypeID);
+                }
+            }
+        }
+
+        private int GetTechnicianIDByAccount(string account)
+        {
+            int id = -1;
+
+            // 直接使用設計器裡拉好的物件，不用new
+            this.techniciansTableAdapter1.FillByAccount(this.t_ACDataSet1.Technicians, account);
+
+            if (this.t_ACDataSet1.Technicians.Rows.Count > 0)
+            {
+                //把我要的師傅id撈出來
+                id = this.t_ACDataSet1.Technicians[0].T_id;
+            }
+
+            return id;
+        }
+
+        //再從訂單表 用T_id撈對應的訂單資料 先載入所有資料包含所有工單類型
+        private void LoadOrders(int techID)
+        {
+            //把ID傳入到workOrder 把order資料撈出來
+            this.workOrderTableAdapter1.FillByTechnicianID(this.t_ACDataSet1.WorkOrder, techID);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.workOrderBindingSource.Position = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.workOrderBindingSource.Position -= 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.workOrderBindingSource.Position += 1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //最後一筆資料
+            this.workOrderBindingSource.Position = this.workOrderBindingSource.Count - 1;
+        }
+
+        // 綁定在 BindingSource 物件的事件。
+        // 當 BindingSource 目前所指向的「目前那一筆資料」改變時，會觸發 CurrentChanged 事件。
+        // 也就是說：只要資料指標 (Position) 改變，CurrentChanged 就會被觸發。
+        // 例如：按按鈕改變 Position、或是滑鼠直接點選 DataGridView 其他列，都會觸發此事件。
+        // 注意：如果只是修改目前那筆資料的內容，但 Position 沒有改變，則不會觸發此事件。
+        // 只負責監聽位置移動 (Position 改變)
+        private void workOrderBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+            //設定好label 顯示 第幾筆/ 共有幾筆
+            this.workOrderlabel.Text = $"{this.workOrderBindingSource.Position + 1} / {this.workOrderBindingSource.Count}";
         }
     }
 }
