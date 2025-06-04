9612327350efdbcea744767cb0ef113a44760399
Wayne <dick29984229@gmail.com>
2025-06-05 00:12:50 +0800

    新增上傳照片

--- prjAircondition/Repair/UserControl/R_ucPhotoUpload.Designer.cs
+++ prjAircondition/Repair/UserControl/R_ucPhotoUpload.Designer.cs
@@ -28,10 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.RE_dgvPhotoList = new System.Windows.Forms.DataGridView();
+            this.RE_pbPreview = new System.Windows.Forms.PictureBox();
+            this.RE_btnUpload = new System.Windows.Forms.Button();
+            this.RE_btnDelete = new System.Windows.Forms.Button();
+            this.RE_txtDescription = new System.Windows.Forms.TextBox();
+            this.RE_lblCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.RE_dgvPhotoList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RE_pbPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RE_dgvPhotoList
+            // 
+            this.RE_dgvPhotoList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RE_dgvPhotoList.Location = new System.Drawing.Point(171, 343);
+            this.RE_dgvPhotoList.Name = "RE_dgvPhotoList";
+            this.RE_dgvPhotoList.RowHeadersWidth = 51;
+            this.RE_dgvPhotoList.RowTemplate.Height = 24;
+            this.RE_dgvPhotoList.Size = new System.Drawing.Size(701, 313);
+            this.RE_dgvPhotoList.TabIndex = 0;
+            this.RE_dgvPhotoList.SelectionChanged += new System.EventHandler(this.RE_dgvPhotoList_SelectionChanged);
+            // 
+            // RE_pbPreview
+            // 
+            this.RE_pbPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.RE_pbPreview.Location = new System.Drawing.Point(171, 29);
+            this.RE_pbPreview.Name = "RE_pbPreview";
+            this.RE_pbPreview.Size = new System.Drawing.Size(701, 174);
+            this.RE_pbPreview.TabIndex = 1;
+            this.RE_pbPreview.TabStop = false;
+            // 
+            // RE_btnUpload
+            // 
+            this.RE_btnUpload.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.RE_btnUpload.Location = new System.Drawing.Point(264, 225);
+            this.RE_btnUpload.Name = "RE_btnUpload";
+            this.RE_btnUpload.Size = new System.Drawing.Size(98, 38);
+            this.RE_btnUpload.TabIndex = 2;
+            this.RE_btnUpload.Text = "上傳";
+            this.RE_btnUpload.UseVisualStyleBackColor = true;
+            this.RE_btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // RE_btnDelete
+            // 
+            this.RE_btnDelete.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.RE_btnDelete.Location = new System.Drawing.Point(260, 281);
+            this.RE_btnDelete.Name = "RE_btnDelete";
+            this.RE_btnDelete.Size = new System.Drawing.Size(102, 38);
+            this.RE_btnDelete.TabIndex = 2;
+            this.RE_btnDelete.Text = "刪除選擇";
+            this.RE_btnDelete.UseVisualStyleBackColor = true;
+            this.RE_btnDelete.Click += new System.EventHandler(this.RE_btnDelete_Click);
+            // 
+            // RE_txtDescription
+            // 
+            this.RE_txtDescription.Location = new System.Drawing.Point(611, 225);
+            this.RE_txtDescription.Name = "RE_txtDescription";
+            this.RE_txtDescription.Size = new System.Drawing.Size(100, 22);
+            this.RE_txtDescription.TabIndex = 3;
+            // 
+            // RE_lblCount
+            // 
+            this.RE_lblCount.AutoSize = true;
+            this.RE_lblCount.Font = new System.Drawing.Font("新細明體", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.RE_lblCount.Location = new System.Drawing.Point(626, 281);
+            this.RE_lblCount.Name = "RE_lblCount";
+            this.RE_lblCount.Size = new System.Drawing.Size(85, 34);
+            this.RE_lblCount.TabIndex = 4;
+            this.RE_lblCount.Text = "筆數";
+            // 
+            // R_ucPhotoUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.RE_lblCount);
+            this.Controls.Add(this.RE_txtDescription);
+            this.Controls.Add(this.RE_btnDelete);
+            this.Controls.Add(this.RE_btnUpload);
+            this.Controls.Add(this.RE_pbPreview);
+            this.Controls.Add(this.RE_dgvPhotoList);
+            this.Name = "R_ucPhotoUpload";
+            this.Size = new System.Drawing.Size(1017, 699);
+            ((System.ComponentModel.ISupportInitialize)(this.RE_dgvPhotoList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RE_pbPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView RE_dgvPhotoList;
+        private System.Windows.Forms.PictureBox RE_pbPreview;
+        private System.Windows.Forms.Button RE_btnUpload;
+        private System.Windows.Forms.Button RE_btnDelete;
+        private System.Windows.Forms.TextBox RE_txtDescription;
+        private System.Windows.Forms.Label RE_lblCount;
     }
 }
--- prjAircondition/Repair/UserControl/R_ucPhotoUpload.cs
+++ prjAircondition/Repair/UserControl/R_ucPhotoUpload.cs
@@ -2,19 +2,153 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Drawing;
 
 namespace prjAircondition.Repair
 {
     public partial class R_ucPhotoUpload : UserControl
     {
+        private string connStr = "Data Source=.;Initial Catalog=AC;Integrated Security=True;";
+        private int currentWorkOrderID = 1; // 可改成傳入的 ID
+        private DataTable photoTable;
+        private SqlDataAdapter adapter;
         public R_ucPhotoUpload()
         {
             InitializeComponent();
+            RE_btnUpload.Click += btnUpload_Click;
+            RE_btnDelete.Click += RE_btnDelete_Click;
+            RE_dgvPhotoList.SelectionChanged += RE_dgvPhotoList_SelectionChanged;
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "圖片檔 (*.jpg;*.png;*.bmp)|*.jpg;*.png;*.bmp";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = dialog.FileName;
+                byte[] photoBytes = File.ReadAllBytes(filePath);
+
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    string sql = @"
+                INSERT INTO WorkOrderPhoto (WorkOrderID, PhotoData, PhotoDescription, UploadTime)
+                VALUES (@WorkOrderID, @PhotoData, @Description, @UploadTime)";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@WorkOrderID", currentWorkOrderID);
+                        cmd.Parameters.AddWithValue("@PhotoData", photoBytes);
+                        cmd.Parameters.AddWithValue("@Description", RE_txtDescription.Text.Trim());
+                        cmd.Parameters.AddWithValue("@UploadTime", DateTime.Now);
+
+                        try
+                        {
+                            conn.Open();
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show(" 照片已成功上傳！");
+                            LoadPhotoList(); // 重新整理
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("上傳失敗：" + ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void LoadPhotos()
+        {
+            string sql = "SELECT PhotoID, PhotoDescription, UploadTime FROM WorkOrderPhoto WHERE WorkOrderID = @WorkOrderID";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.Parameters.AddWithValue("@WorkOrderID", currentWorkOrderID);
+                adapter = new SqlDataAdapter(cmd);
+                photoTable = new DataTable();
+                adapter.Fill(photoTable);
+                RE_dgvPhotoList.DataSource = photoTable;
+                RE_lblCount.Text = $"共 {photoTable.Rows.Count} 筆";
+            }
+        }
+
+
+        private void LoadPhotoList()
+        {
+            string sql = "SELECT PhotoID, PhotoDescription, UploadTime FROM WorkOrderPhoto WHERE WorkOrderID = @WorkOrderID";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.Parameters.AddWithValue("@WorkOrderID", currentWorkOrderID);
+                adapter = new SqlDataAdapter(cmd);
+                photoTable = new DataTable();
+                adapter.Fill(photoTable);
+
+                RE_dgvPhotoList.DataSource = photoTable;
+                RE_lblCount.Text = $"共 {photoTable.Rows.Count} 筆";
+            }
+        }
+
+
+
+        private void R_ucPhotoUpload_Load(object sender, EventArgs e)
+        {
+            LoadPhotos();
+        }
+
+        private void RE_btnDelete_Click(object sender, EventArgs e)
+        {
+            if (RE_dgvPhotoList.CurrentRow == null)
+            {
+                MessageBox.Show("請先選取一筆照片");
+                return;
+            }
+
+            int photoId = Convert.ToInt32(RE_dgvPhotoList.CurrentRow.Cells["PhotoID"].Value);
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand("DELETE FROM WorkOrderPhoto WHERE PhotoID = @PhotoID", conn))
+            {
+                cmd.Parameters.AddWithValue("@PhotoID", photoId);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("✅ 照片已刪除");
+            LoadPhotos();
+        }
+
+        private void RE_dgvPhotoList_SelectionChanged(object sender, EventArgs e)
+        {
+            if (RE_dgvPhotoList.CurrentRow == null) return;
+
+            int photoId = Convert.ToInt32(RE_dgvPhotoList.CurrentRow.Cells["PhotoID"].Value);
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand("SELECT PhotoData FROM WorkOrderPhoto WHERE PhotoID = @PhotoID", conn))
+            {
+                cmd.Parameters.AddWithValue("@PhotoID", photoId);
+                conn.Open();
+                byte[] data = cmd.ExecuteScalar() as byte[];
+                if (data != null)
+                {
+                    using (MemoryStream ms = new MemoryStream(data))
+                    {
+                        RE_pbPreview.Image = Image.FromStream(ms);
+                    }
+                }
+            }
         }
     }
 }
