cfd58a9e114f793f6183b1e0a16fc942a0bc6c9b
kokorokara <508170117@m365.fju.edu.tw>
2025-06-04 11:05:17 +0800

    新增formtech

--- prjAircondition/Program.cs
+++ prjAircondition/Program.cs
@@ -13,7 +13,7 @@ namespace prjAircondition
         /// 應用程式的主要進入點。
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
